a1825da1b894e353d64425aac02340c9da2458f0
ZoltanMG <zoltanmoragarcia@gmail.com>
2023-06-01 20:32:53 -0500

    test

--- python/suma/0-suma.py
+++ python/suma/0-suma.py
@@ -3,5 +3,5 @@ def suma(numero_1, numero_2):
         value_1 = int(numero_1)
         value_2 = int(numero_2)
     except:
-        return "Error: ingrese un valor valido."
+        return "Error: ingrese un valor valido"
     return value_1 + value_2
