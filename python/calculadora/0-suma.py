fcf9341dfdfed7d0d4d90cfa190e8d4a04f481ee
Zoltán MG <YX758pO0+A$%*Zd0m6)Gi>
2023-12-31 19:01:17 -0500

    clean

--- python/calculadora/0-suma.py
+++
@@ -1,7 +0,0 @@
-def suma(numero_1, numero_2):
-    try:
-        value_1 = int(numero_1)
-        value_2 = int(numero_2)
-    except:
-        return "Error: ingrese un valor valido"
-    return value_1 + value_2
